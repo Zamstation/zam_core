b533c5cf3520039f674de48ebe9e6f545f0e9c28
amsakanna <amsakanna@gmail.com>
2021-05-24 14:51:53 +0530

    Modify Builders and Callbacks Modified Builder and ParameterizedBuilder generics to accept nulls. Modified all Callbacks generics to accept nulls.

--- lib/src/callback/parameterized_callback.typedef.dart
+++ lib/src/callback/parameterized_callback.typedef.dart
@@ -5,5 +5,5 @@
 ///     ...
 ///     final price = roundOffStrategy(8.458);
 ///
-typedef ParameterizedCallback<INPUT extends Object, OUTPUT extends Object>
+typedef ParameterizedCallback<INPUT extends Object?, OUTPUT extends Object?>
     = OUTPUT Function(INPUT input);
