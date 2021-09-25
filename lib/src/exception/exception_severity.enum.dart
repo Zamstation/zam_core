0bf1398abb87243bd4126cf92cb16f5d8342e899
amsakanna <amsakanna@gmail.com>
2021-09-25 20:54:41 +0530

    Feature: ExceptionSeverity.fatal Fixes #23

--- lib/src/exception/exception_severity.enum.dart
+++ lib/src/exception/exception_severity.enum.dart
@@ -1,5 +1,14 @@
+///
+/// [ExceptionSeverity] is an enum with the following values.
+///
+/// - none -> if you don't want to set any severity.
+/// - warning -> exception that require minor fixes.
+/// - critical -> exception that require taking alternate paths.
+/// - fatal -> exceptions that cannot be fixed.
+///
 enum ExceptionSeverity {
   none,
   warning,
   critical,
+  fatal,
 }
